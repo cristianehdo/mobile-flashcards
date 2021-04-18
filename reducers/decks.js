53a8ba5cd74813abf7a67f901c495b96ca05e216
cristianehdo <cristiane.hdo@gmail.com>
2021-04-18 21:33:46 +0100

    fix: decks reducer

---
+++ reducers/decks.js
@@ -0,0 +1,39 @@
+import {
+  RECEIVE_DECKS,
+  ADD_DECK,
+  REMOVE_DECK,
+  ADD_CARD} from '../actions'
+
+const decks = (state = {}, action) => {
+  switch (action.type) {
+  case RECEIVE_DECKS :
+    return {
+      ...state,
+      ...action.decks,
+    }
+  case ADD_DECK :
+    return {
+      ...state,
+      [action.deck.id]: {
+        ...action.deck,
+      },
+    }
+  case REMOVE_DECK: {
+    const decksObj = { ...state }
+    delete decksObj[action.id]
+    return decksObj
+  }
+  case ADD_CARD :
+    return {
+      ...state,
+      [action.deckId]: {
+        ...action.decks[action.deckId],
+        ...action.deck.cards.concat([action.card])
+      },
+    }
+  default :
+    return state
+  }
+}
+
+export default decks
