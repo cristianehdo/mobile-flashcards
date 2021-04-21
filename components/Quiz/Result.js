67af6af18e3281022aff074facc19b69a5e89c82
cristianehdo <cristiane.hdo@gmail.com>
2021-04-21 14:19:21 +0100

    feat: Add result styles

--- components/Quiz/Result.js
+++ components/Quiz/Result.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View, Text } from 'react-native'
-import TextButton from './TextButton'
+import { View, Text, StyleSheet } from 'react-native'
+import TextButton from '../TextButton'
 
-const Result = ({styles, score, total, onPress}) => {
+const Result = ({score, total, onPress}) => {
   return(
     <View style={styles.container} >
       <View>
         <Text style={styles.text}>
+          Result:
+        </Text>
+        <Text style={styles.text}>
           You got it rigth {score} out of {total} questions.
         </Text>
       </View>
-      <TextButton style={{fontSize: 16}} onPress={onPress}>
+      <TextButton style={styles.button} onPress={onPress}>
         Reset Quiz
       </TextButton>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 20,
+    alignSelf: 'center',
+    margin: 5,
+  },
+  button: {
+    fontSize: 16,
+    margin: 20,
+  }
+})
 Result.propTypes = {
-  styles: PropTypes.object,
   score: PropTypes.number,
   total: PropTypes.number,
-  onPress: PropTypes.fun,
+  onPress: PropTypes.func,
 }
 export default Result
